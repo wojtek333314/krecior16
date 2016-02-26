264d8521b626979a32dcad4706a5af6648a73823
Grzesiek <epolakko@o2.pl>
2016-02-26 15:02:16 +0100

    POZDRAWIAM <3

--- desktop/src/com/krecior/desktop/DesktopLauncher.java
+++ desktop/src/com/krecior/desktop/DesktopLauncher.java
@@ -1,15 +1,29 @@
 package com.krecior.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.krecior.Manager;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 540;
 		config.height = 960;
+
+		/**
+		 *  .----------------.  .----------------.  .----------------.  .----------------.
+		 | .--------------. || .--------------. || .--------------. || .--------------. |
+		 | |     ____     | || |    _______   | || |      __      | || |  ___  ____   | |
+		 | |   .'    `.   | || |   /  ___  |  | || |     /  \     | || | |_  ||_  _|  | |
+		 | |  /  .--.  \  | || |  |  (__ \_|  | || |    / /\ \    | || |   | |_/ /    | |
+		 | |  | |    | |  | || |   '.___`-.   | || |   / ____ \   | || |   |  __'.    | |
+		 | |  \  `--'  /  | || |  |`\____) |  | || | _/ /    \ \_ | || |  _| |  \ \_  | |
+		 | |   `.____.'   | || |  |_______.'  | || ||____|  |____|| || | |____||____| | |
+		 | |              | || |              | || |              | || |              | |
+		 | '--------------' || '--------------' || '--------------' || '--------------' |
+		 '----------------'  '----------------'  '----------------'  '----------------'
+		 *
+		 */
 		
-		new LwjglApplication(new Manager(null), config);
+		//new LwjglApplication(new Manager(null), config);
+
 	}
 }
